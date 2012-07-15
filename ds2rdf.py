d1f9af3de6bc5d35359b7f75ea6fc5f1fde7603e
Alvaro Graves <alvaro@graves.cl>
2012-07-15 11:01:40 -0700

    Added several triples (owl:sameAs, dcat:catalog, etc.)

--- ds2rdf.py
+++ ds2rdf.py
@@ -5,21 +5,43 @@ from rdflib.namespace import Namespace, RDF, XSD
 import csv
 import sys
 import re
+import json
 
 
 class Ds2rdf:
-  def __init__(self, csvFile, root = 'http://data.rpi.edu/id/'):
+  def __init__(self, csvFile, root = 'http://data.rpi.edu/'):
     try:
       self.store = None
       self.rootUri = root
       self.source = csvFile
+      self.collectionTree = {}
       self.reader = csv.reader(open(csvFile, 'rb'), delimiter=',')
+      self.semanticHeaders = [None]* 10
+      
+      self.readCollectionTree()
     except IOError as (errno, strerror):
       print >> sys.stderr, "I/O error({0}): {2} {1}".format(errno, strerror, csvFile)
       exit(1)
       
+  def readCollectionTree(self):
+    self.collectionTree["123456789/2"] = "community/twc/collection/logd" 
+    self.collectionTree["123456789/6"] = "community/humanities/collection/psychology" 
+    self.collectionTree["123456789/7"] = "community/humanities/collection/sociology" 
+    
   def cleanLiteral(self, literal):
     return literal.title().replace(".", "").replace(" ", "_")
+    
+  def getDatasetUri(self, row):
+    if row[1] in self.collectionTree:
+      collection  = self.collectionTree[row[1]]
+    else:
+      print >> sys.stderr, "Can't find collection string for %s" % row[1]
+      exit(0)
+    uri = self.rootUri+collection+'/dataset/'+self.cleanLiteral( URIRef(row[self.semanticHeaders.index("title")]))
+    return uri
+  
+  def getCollectionUri(self, colId):
+    return 'http://hdl.handle.net/'+colId
     
   def subjectProcessor(self, root, value):
     DC = Namespace("http://purl.org/dc/elements/1.1/")
@@ -35,18 +57,27 @@ class Ds2rdf:
       
   def authorProcessor(self, root, value):
     DC = Namespace("http://purl.org/dc/elements/1.1/")
+    RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+    FOAF = Namespace("http://xmlns.com/foaf/0.1/")
+    RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+    OWL = Namespace("http://www.w3.org/2002/07/owl#")
     a = value.split("||")
     for i in a:
       contrib = None
       if re.match("^(http:\/\/)", i) != None:
-        contrib = i
+        contrib = self.rootUri+'contributor/'+i.replace("://", "/")
+        self.store.add((URIRef(contrib), RDFS['label'], Literal(i) ))     
+        self.store.add((URIRef(contrib), OWL['sameAs'], URIRef(i) ))     
       else:
         aux = i.split(", ")
         if len(aux)>1:
-          contrib = self.rootUri+self.cleanLiteral(aux[1])+self.cleanLiteral(aux[0])
+          contrib = self.rootUri+'contributor/'+self.cleanLiteral(aux[1])+self.cleanLiteral(aux[0])
+          self.store.add((URIRef(contrib), RDFS['label'], Literal(aux[1]+" "+aux[0]) ))     
         else:                     
-          contrib = self.rootUri+self.cleanLiteral(aux[0])
+          contrib = self.rootUri+'contributor/'+self.cleanLiteral(aux[0])
+          self.store.add((URIRef(contrib), RDFS['label'], Literal(aux[0]) ))     
       self.store.add((root, DC['contributor'], URIRef(contrib)))     
+      self.store.add((URIRef(contrib), RDF['type'], FOAF['Agent'] ))     
       
   def coverageProcessor(self, root, value):
     DC = Namespace("http://purl.org/dc/elements/1.1/")
@@ -62,10 +93,20 @@ class Ds2rdf:
 
   def titleProcessor(self, root, value):
     DC = Namespace("http://purl.org/dc/elements/1.1/")
+    DCAT = Namespace("http://www.w3.org/ns/dcat#")
+    RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+    RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
     a = value.split("||")
     for i in a:
       self.store.add((root, DC['title'], Literal(i)))     
+      self.store.add((root, RDFS['label'], Literal(i)))     
+      self.store.add((root, RDF['type'], DCAT['Dataset']))    
 
+
+  def identifierProcessor(self, root, value):
+    OWL = Namespace("http://www.w3.org/2002/07/owl#")
+    self.store.add((URIRef(root), OWL['sameAs'], URIRef(value)))
+  
   def convert(self):
     self.store = Graph()
     processors = {}
@@ -75,46 +116,50 @@ class Ds2rdf:
     processors['contributor'] = self.authorProcessor
     processors['coverage.spatial'] = self.coverageProcessor
     processors['description.abstract'] = self.descriptionProcessor
+    processors['identifier.uri'] = self.identifierProcessor
     processors['title'] = self.titleProcessor
     self.store.bind("dc", "http://purl.org/dc/elements/1.1/")
     self.store.bind("data", "http://data.rpi.edu/vocab/")
     self.store.bind("owl", "http://www.w3.org/2002/07/owl#")
     self.store.bind("rdfs", "http://www.w3.org/2000/01/rdf-schema#")
     self.store.bind("foaf", "http://xmlns.com/foaf/0.1/")
-    self.store.bind("void", "http://rdfs.org/ns/void#")
+    self.store.bind("dcat", "http://www.w3.org/ns/dcat#")
+    RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
     DC = Namespace("http://purl.org/dc/elements/1.1/")
     OWL = Namespace("http://www.w3.org/2002/07/owl#")
     DATA = Namespace("http://data.rpi.edu/vocab/")
     RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
     FOAF = Namespace("http://xmlns.com/foaf/0.1/")
-    VOID = Namespace("http://rdfs.org/ns/void#")
+    DCAT = Namespace("http://www.w3.org/ns/dcat#")
     header = self.reader.next()   #Skip header  
     minSize = len(header)
-    semanticHeaders = [None]* len(header)
+    self.semanticHeaders = [None]* len(header)
     headerCounter = 0
     for i in header:
       #Take only DC terms
       if re.match("^(dc\.)", i) != None:
         normalizedHeader = re.sub("\[\w+\]$", "", i)
         normalizedHeader = re.sub("^dc\.", "", normalizedHeader)
-        semanticHeaders[headerCounter] = normalizedHeader
-        currentList = semanticHeaders[headerCounter]
+        self.semanticHeaders[headerCounter] = normalizedHeader
+        currentList = self.semanticHeaders[headerCounter]
         currentList = normalizedHeader
       headerCounter += 1
-
     for row in self.reader:
       if len(row) != minSize:
         print >> sys.stderr,  "Number of columns different than header ({0} vs. {1}). Skipping".format(len(row), minSize)
         exit(1) #continue
-      datasetUri = URIRef(row[semanticHeaders.index("identifier.uri")])
+      datasetUri = self.getDatasetUri(row)
+      collectionUri = self.getCollectionUri(row[1])
+      self.store.add((URIRef(collectionUri), DCAT['dataset'], URIRef(datasetUri) ))     
+      self.store.add((URIRef(collectionUri), RDF['type'], DCAT['Catalog'] ))     
       for index, cell in enumerate(row):
         if cell != "":
-          if semanticHeaders[index] in processors:
-            aux = processors[semanticHeaders[index]]
+          if self.semanticHeaders[index] in processors:
+            aux = processors[self.semanticHeaders[index]]
             aux(datasetUri, cell)
 #          else:
-#            self.store.add((datasetUri, DC[str(semanticHeaders[index])], Literal(cell)))
-    print(self.store.serialize(format="turtle"))
+#            self.store.add((datasetUri, DC[str(self.semanticHeaders[index])], Literal(cell)))
+    print(self.store.serialize(format="pretty-xml"))
     
 if len(sys.argv) < 2:
   print >> sys.stderr, "No file selected"
